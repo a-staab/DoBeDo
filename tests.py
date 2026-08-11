bdb71d5de6d1b3182e1144d83dc92cc4f2705a31
a-staab <a-staab@users.noreply.github.com>
2026-08-11 13:45:23 -0500

    Just one attempt to bind SQLAlchemy to app

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 from server import app
 from model import db, connect_to_db, example_data
 import unittest
-import os
 
 class LoggedOut_Page_Loads(unittest.TestCase):
 
@@ -27,100 +26,69 @@ class LoggedOut_Page_Loads(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'to your account', result.data)
 
-# class Test(unittest.TestCase):
-#     """Tests for public pages."""
 
-#     def setUp(self):
-#         """Do this before each test."""
+class Logged_In(unittest.TestCase):
+    """Tests for pages requiring sign in."""
 
-#         self.client = app.test_client()
-#         app.config['TESTING'] = True
+    @classmethod
+    def setUpClass(cls):
+        """Just once."""
+        app.config['TESTING'] = True
+        app.config['SECRET_KEY'] = 'key'
+        connect_to_db(app, "postgresql:///test_database")
 
-#         # Connect to test database
-#         connect_to_db(app, os.environ.get('TEST_DB_URI'))
+    def setUp(self):
+        """Before each test."""
+        self.client = app.test_client()
+        # Store a value for user_id in the session to mimic signed-in user
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['user_id'] = 1
+        # Create tables and add sample data
+        db.create_all()
+        example_data()
 
-#         # Create tables and add sample data
-#         db.create_all()
-#         example_data()
+    def tearDown(self):
+        """After every test."""
+        db.session.close()
+        db.drop_all()
 
-#     def tearDown(self):
-#         """Do this after every test."""
+    def test_setup_page_renders(self):
+        """Tests activity setup page loads."""
+        result = self.client.get("/setup")
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'usually ideal', result.data)
 
-#         # End session and delete tables
-#         db.session.close()
-#         db.drop_all()
+    def test_create_activity_types(self):
+        """Tests user can specify an activity type for tracking using every
+        available field on the setup page."""
+        result = self.client.post("/setup",
+                                  data={"activity_1": "coding",
+                                        "activity_2": "sports",
+                                        "activity_3": "shopping",
+                                        "activity_4": "friends",
+                                        "activity_5": "studying",
+                                        "activity_6": "meditation",
+                                        "activity_7": "family",
+                                        "activity_8": "napping",
+                                        "activity_9": "piano",
+                                        "activity_10": "writing"},
+                                  follow_redirects=True)
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'Plan an Activity', result.data)
 
+    def test_main_page_render(self):
+        """Tests that main page loads."""
+        result = self.client.get("/main")
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'activity to begin tracking', result.data)
 
+    # def test_create_new_user(self):
+    # """Tests database for existence of user"""
 
-#     # def test_create_new_user(self):
-#     #     """Tests database for existence of user created using create_new_user
-#     #     helper function."""
+    # result = User.query.filter(User.email == 'hb-student@hackbright.com').one()
+    # self.assertIn('with user_id', result.data)
 
-#     #     result = User.query.filter(User.email == 'hb-student@hackbright.com').one()
-#     #     self.assertIn('with user_id', result.data)
-
-
-# class Test_Signed_In(unittest.TestCase):
-#     """Tests for pages requiring sign in."""
-
-#     def setUp(self):
-#         """Do this before each test."""
-
-#         self.client = app.test_client()
-#         app.config['TESTING'] = True
-#         app.config['SECRET_KEY'] = 'key'
-
-#         # Store a value for user_id in the session to mimic signed-in user
-#         with self.client as c:
-#             with c.session_transaction() as sess:
-#                 sess['user_id'] = 1
-
-#         # Connect to test database
-#         connect_to_db(app, "postgresql:///test_database")
-
-#         # Create tables and add sample data
-#         db.create_all()
-#         example_data()
-
-#     def tearDown(self):
-#         """Do this after every test."""
-
-#         # End session and delete tables
-#         db.session.close()
-#         db.drop_all()
-
-#     def test_request_activity_types(self):
-#         """Tests activity setup page loads."""
-
-#         result = self.client.get("/setup")
-#         self.assertEqual(result.status_code, 200)
-#         self.assertIn('usually ideal', result.data)
-
-#     def test_create_activity_types(self):
-#         """Tests user can specify an activity type for tracking using every
-#         available field on the setup page."""
-
-#         result = self.client.post("/setup",
-#                                   data={"activity_1": "coding",
-#                                         "activity_2": "sports",
-#                                         "activity_3": "shopping",
-#                                         "activity_4": "friends",
-#                                         "activity_5": "studying",
-#                                         "activity_6": "meditation",
-#                                         "activity_7": "family",
-#                                         "activity_8": "napping",
-#                                         "activity_9": "piano",
-#                                         "activity_10": "writing"},
-#                                   follow_redirects=True)
-#         self.assertEqual(result.status_code, 200)
-#         self.assertIn('Plan an Activity', result.data)
-
-#     def test_main_page_render(self):
-#         """Tests that main page loads."""
-
-#         result = self.client.get("/main")
-#         self.assertEqual(result.status_code, 200)
-#         self.assertIn("activity to begin tracking", result.data)
     
 if __name__ == '__main__':
     unittest.main()
